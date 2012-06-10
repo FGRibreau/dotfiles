7c30815227a4aecc9d0b4d731a3e406fdfd8e6ff
FG Ribreau <github@fgribreau.com>
2012-06-10 14:26:56 +0100

    Bug fix

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PWD=$(dirname $(which $0))
+
 echo "initializing submodules"
 git submodule init
 git submodule update
@@ -15,20 +17,20 @@ rm ~/.gitignore
 rm ~/.tmux.conf
 
 echo "Symlinking files"
-ln -s ~/Dropbox/OS/dotfiles/vimrc ~/.vimrc
-ln -s ~/Dropbox/OS/dotfiles/vim ~/.vim
+ln -s ${PWD}/vimrc ~/.vimrc
+ln -s ${PWD}/vim ~/.vim
 
-ln -s ~/Dropbox/OS/dotfiles/zshrc ~/.zshrc
-ln -s ~/Dropbox/OS/dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ${PWD}/zshrc ~/.zshrc
+ln -s ${PWD}/oh-my-zsh ~/.oh-my-zsh
 
-ln -s ~/Dropbox/OS/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/Dropbox/OS/dotfiles/gitignore ~/.gitignore
-ln -s ~/Dropbox/OS/dotfiles/tmux ~/.tmux.conf
+ln -s ${PWD}/gitconfig ~/.gitconfig
+ln -s ${PWD}/gitignore ~/.gitignore
+ln -s ${PWD}/tmux ~/.tmux.conf
 
 echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
 
 echo "Path zsh-theme"
-ln -s ~/Dropbox/OS/dotfiles/fg.zsh-theme ~/.oh-my-zsh/themes/fg.zsh-theme
+ln -s ${PWD}/fg.zsh-theme ~/.oh-my-zsh/themes/fg.zsh-theme
 
 echo "All done."
